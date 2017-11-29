b08f02db0c0b74ad6ffb33f62fc70c5f26164944
v2049 <v2049@10.111.93.228>
2017-11-29 17:32:55 -0500

    Created determinator class

---
+++ src/MatrixWorker/Determinator.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package MatrixWorker;
+
+import Data.Matrix;
+
+/**
+ *
+ * @author v2049
+ */
+public class Determinator {
+    
+    public Number executeDetermination(Matrix matrix){
+        return 0;
+    }
+}
